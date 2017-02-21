9c34e61e6b20539bb9757b62b427823de93cf690
ariwaranosai <nkssai@outlook.com>
2017-02-21 20:20:43 +0800

    update store

--- src/store.js
+++ src/store.js
@@ -9,22 +9,6 @@ export default new Vue({
         updateData: function(d) {
             this.data = d;
             this.isReady = true;
-        },
-        getByDay: function(day) {
-            return this.data[week2num(day)];
-        },
-        week2num: function(key) {
-            let m = {
-            "星期日": 0,
-            "星期一": 1,
-            "星期二": 2,
-            "星期三": 3,
-            "星期四": 4,
-            "星期五": 5,
-            "星期六": 6,
-            "其他": -1
-            }
-            return m[key];
         }
-    }
+   }
 })
